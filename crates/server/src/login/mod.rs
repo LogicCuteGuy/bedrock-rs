581099b3e0e9b9db37524b1d09e462858d4fb016
LogicCuteGuy <ohm@logiccuteguy.com>
2025-04-15 18:10:56 +0700

    aaaa

--- crates/server/src/login/mod.rs
+++ crates/server/src/login/mod.rs
@@ -7,11 +7,11 @@ use bedrockrs_proto::connection::Connection;
 use bedrockrs_proto::v729::helper::ProtoHelperV729;
 
 pub async fn login(
-    connection: Connection,
+    connection: Connection<ProtoHelperV729>,
     // world: &mut World,
     login_handler: impl LoginHandler,
 ) -> Result<(), LoginError> {
-    let mut shard = shard::<ProtoHelperV729>(connection);
+    let mut shard = shard(connection);
 
     todo!()
 }
